cdaa78896c0900aebc7f41822ab267973e60b33b
mendiola10 <88906590+mendiola10@users.noreply.github.com>
2021-11-29 23:08:24 -0600

    Update encode.ps1

--- practica6/encode.ps1
+++ practica6/encode.ps1
@@ -1,4 +1,4 @@
-$archivo = Get-Content .\transcript.txt
+$archivo = Get-Content .\transcripcion.txt
 
 $ste = [System.Text.Encoding]::UTF8.GetBytes($archivo)
-[System.Convert]::ToBase64String($ste) > .\transcripcion.txt.txt
+[System.Convert]::ToBase64String($ste) > .\transcripcion.txt
